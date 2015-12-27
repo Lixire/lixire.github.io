aeee2a40a9ae166d0b04e5294e9be45595373bb8
Amy <publishedhere@gmail.com>
2015-12-27 01:23:08 -0500

    Old Stuff

--- DATABEST/mysqlconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("HOST", "mysql13.000webhost.com");
-    define("USER", "a1608365_admin");
-    define("PASSWORD", "jadasbinich1337");
-    define("DATABASE", "a1608365_testing");
-?>
